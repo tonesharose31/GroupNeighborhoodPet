eb9d83cb644914206457d8fb928178f10258ea2a
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-09-24 04:37:03 -0400

    imported componted inton app jsx and created logic for routing

---
+++ src/components/PetDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PetDetail = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PetDetail;
---
+++ src/components/PetForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PetForm = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PetForm;
